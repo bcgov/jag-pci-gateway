2cb41d242515ef368e3224479c8bd877fd6f3df7
alex <alexandre.joyeux@nttdata.com>
2020-11-16 13:07:13 -0800

    enable all certificates

--- src/pci-gateway/src/main/java/ca/bc/gov/open/jag/pcigateway/PciGatewayApplication.java
+++ src/pci-gateway/src/main/java/ca/bc/gov/open/jag/pcigateway/PciGatewayApplication.java
@@ -6,13 +6,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import javax.net.ssl.HttpsURLConnection;
+
 @SpringBootApplication
 @EnableEncryptableProperties
 @EnableConfigurationProperties({AppProperties.class})
 public class PciGatewayApplication {
 
 	public static void main(String[] args) {
+
+		// TODO: remove
+		HttpsURLConnection.setDefaultHostnameVerifier((hostname, session) -> true);
+
 		SpringApplication.run(PciGatewayApplication.class, args);
+
 	}
 
 }
