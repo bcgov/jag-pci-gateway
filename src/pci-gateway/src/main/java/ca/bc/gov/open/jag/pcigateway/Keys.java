ec2581336ef3e4165a635f2955607167fdb6dc14
alex <alexandre.joyeux@nttdata.com>
2020-11-16 14:51:02 -0800

    more refactoring

--- src/pci-gateway/src/main/java/ca/bc/gov/open/jag/pcigateway/Keys.java
+++ src/pci-gateway/src/main/java/ca/bc/gov/open/jag/pcigateway/Keys.java
@@ -4,7 +4,7 @@ public class Keys {
 
     private Keys() {}
 
-    public static final String PARAM_TRANS_HASH_VALUE = "hashValue";
+    public static final String PARAM_HASH_VALUE = "hashValue";
     public static final String PAYMENT_PATH = "scripts/Payment/Payment.asp";
     public static final String PROCESS_TRANSACTION_PATH = "scripts/process_transaction.asp";
 
--- src/pci-gateway/src/main/java/ca/bc/gov/open/jag/pcigateway/api/RedirectController.java
+++ src/pci-gateway/src/main/java/ca/bc/gov/open/jag/pcigateway/api/RedirectController.java
@@ -4,14 +4,13 @@ import ca.bc.gov.open.jag.pcigateway.Keys;
 import ca.bc.gov.open.jag.pcigateway.config.AppProperties;
 import ca.bc.gov.open.jag.pcigateway.config.GatewayClientProperty;
 import ca.bc.gov.open.jag.pcigateway.utils.HttpServletRequestUtils;
+import ca.bc.gov.open.jag.pcigateway.utils.QueryStringUtils;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +21,6 @@ import org.springframework.web.util.UriComponentsBuilder;
 import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.text.MessageFormat;
-import java.util.Arrays;
-import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -57,30 +54,29 @@ public class RedirectController {
     }
 
     private URI processRequest(HttpServletRequest request, String requestPath) throws MissingServletRequestParameterException {
+
         GatewayClientProperty clientProperty = getGatewayClientProperty(request);
 
-        String hashValue = request.getParameter(Keys.PARAM_TRANS_HASH_VALUE);
+        if(StringUtils.isBlank(request.getParameter(Keys.PARAM_HASH_VALUE)))
+            throw new MissingServletRequestParameterException("hashValue","string");
 
-        if(StringUtils.isBlank(hashValue)) throw new MissingServletRequestParameterException("hashValue","string");
+        if(!validateHash(getSecuredQueryString(request), clientProperty.getGatewayHashKey(), request.getParameter(Keys.PARAM_HASH_VALUE)))
+            throw new MissingServletRequestParameterException("Hash", "Hash is invalid");
 
-        if(!validateHash(getSecuredQueryString(request), clientProperty.getGatewayHashKey(), hashValue)) throw new MissingServletRequestParameterException("Hash", "Hash is invalid");
-
-        String newHash = computeHash(getSecuredQueryString(request), clientProperty.getHashKey());
-
-        return UriComponentsBuilder.fromUri(URI.create(MessageFormat.format("{0}/{1}", appProperties.getRedirectUrl(), requestPath)))
-                .queryParams(swapHash(request.getParameterMap(), newHash))
+        return UriComponentsBuilder
+                .fromUri(URI.create(MessageFormat.format("{0}/{1}", appProperties.getRedirectUrl(), requestPath)))
+                .queryParams(QueryStringUtils.setParam(request.getParameterMap(), Keys.PARAM_HASH_VALUE,
+                        computeHash(getSecuredQueryString(request), clientProperty.getHashKey())))
                 .build().toUri();
 
     }
 
     private String getSecuredQueryString(HttpServletRequest request) {
-        return StringUtils.substringBeforeLast(request.getQueryString(), "&" + Keys.PARAM_TRANS_HASH_VALUE);
+        return StringUtils.substringBeforeLast(request.getQueryString(), "&" + Keys.PARAM_HASH_VALUE);
     }
 
     private boolean validateHash(String queryString, String hashKey, String hashValue) {
-        // Compute the hash
         return StringUtils.equalsIgnoreCase(hashValue, computeHash(queryString, hashKey));
-
     }
 
     private GatewayClientProperty getGatewayClientProperty(HttpServletRequest request) throws MissingServletRequestParameterException {
@@ -106,17 +102,4 @@ public class RedirectController {
         return DigestUtils.md5Hex(MessageFormat.format("{0}{1}", value, hashKey)).toUpperCase();
     }
 
-    private MultiValueMap<String, String> swapHash(Map<String, String[]> current, String hash) {
-
-        MultiValueMap<String, String> result = new LinkedMultiValueMap<>();
-
-        current.entrySet()
-                .stream()
-                .forEach(entry -> result.put(entry.getKey(), Arrays.asList(entry.getValue())));
-
-        result.set(Keys.PARAM_TRANS_HASH_VALUE, hash);
-
-        return result;
-
-    }
 }
