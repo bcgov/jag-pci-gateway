ec3ad32ffa69a5deb05ab7519ad74ea09666228e
alex <alexandre.joyeux@nttdata.com>
2020-11-16 14:02:18 -0800

    adding proxy

---
+++ src/pci-gateway/src/main/java/ca/bc/gov/open/jag/pcigateway/config/Proxy.java
@@ -0,0 +1,23 @@
+package ca.bc.gov.open.jag.pcigateway.config;
+
+public class Proxy {
+
+    private String host;
+    private int port;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
