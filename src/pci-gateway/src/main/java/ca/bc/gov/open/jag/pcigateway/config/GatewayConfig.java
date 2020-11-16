1fc1562723b1eba47ab86d3f44540c6d2c625fee
alex <alexandre.joyeux@nttdata.com>
2020-11-16 12:52:46 -0800

    adding gateway configuration

--- src/pci-gateway/src/main/java/ca/bc/gov/open/jag/pcigateway/config/GatewayConfig.java
+++ src/pci-gateway/src/main/java/ca/bc/gov/open/jag/pcigateway/config/GatewayConfig.java
@@ -1,8 +1,11 @@
 package ca.bc.gov.open.jag.pcigateway.config;
 
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,7 +13,16 @@ public class GatewayConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+
+
+        CloseableHttpClient httpClient
+                = HttpClients.custom()
+                .setSSLHostnameVerifier(new NoopHostnameVerifier())
+                .build();
+        HttpComponentsClientHttpRequestFactory requestFactory
+                = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+
         return new RestTemplate(requestFactory);
     }
 
