41cccf89a4fbe43afb91a6615e54741323898f42
alex <alexandre.joyeux@nttdata.com>
2020-11-16 11:55:45 -0800

    reading scripts

--- src/pci-gateway/src/main/java/ca/bc/gov/open/jag/pcigateway/Keys.java
+++ src/pci-gateway/src/main/java/ca/bc/gov/open/jag/pcigateway/Keys.java
@@ -5,7 +5,7 @@ public class Keys {
     private Keys() {}
 
     public static final String PARAM_TRANS_HASH_VALUE = "hashValue";
-    public static final String PAYMENT_PATH = "Payment/Payment.asp";
-    public static final String PROCESS_TRANSACTION_PATH = "process_transaction.asp";
+    public static final String PAYMENT_PATH = "scripts/Payment/Payment.asp";
+    public static final String PROCESS_TRANSACTION_PATH = "scripts/process_transaction.asp";
 
 }
