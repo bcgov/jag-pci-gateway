77331af7c4364a413a8b65313c8ec17ebbdb34be
taylorclausen <taylor.clausen@nttdata.com>
2021-01-11 08:03:40 -0800

    refactor

--- src/pci-gateway/src/test/java/ca/bc/gov/open/jag/pcigateway/api/rest/PaymentsProxyTest.java
+++ src/pci-gateway/src/test/java/ca/bc/gov/open/jag/pcigateway/api/rest/PaymentsProxyTest.java
@@ -22,7 +22,7 @@ import java.util.List;
 import static org.mockito.ArgumentMatchers.any;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class PaymentsProxyController {
+public class PaymentsProxyTest {
     private static final String MERCHANT_ID = "merchantId";
     private static final String API_URL = "http://localhost:8080";
     public static final String REQUEST_URI = "/pcigw/payments";
