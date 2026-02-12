1129c9980d97d90e1500269fd396638a2ed77b0f
Taylor Clausen <47829178+TayGov@users.noreply.github.com>
2026-02-12 15:03:29 -0800

    Testing/loggin (#118)
    * Add debug logging
    
    * Remove debug logs
    
    * Add additional details

--- src/pci-gateway/src/main/java/ca/bc/gov/open/jag/pcigateway/api/RestProxyController.java
+++ src/pci-gateway/src/main/java/ca/bc/gov/open/jag/pcigateway/api/RestProxyController.java
@@ -96,7 +96,12 @@ public class RestProxyController {
 
         String key = passcode.replace("Passcode ", "");
 
+        logger.info("key {}", key);
+
         String[] keys  = new String(Base64.getDecoder().decode(key)).split(":");
+
+        logger.info("size {}", keys.length);
+        logger.info("postion 1 {}", keys[0]);
 
         if (keys.length != 2) throw new HttpClientErrorException(HttpStatus.UNAUTHORIZED);
 
